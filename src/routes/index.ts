5fa9c9767e3090ef6defc9ace989539bafb429dd
dangtinh <caodangtinh04022003@gmail.com>
2024-12-25 23:00:58 +0700

    feat: post query

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import accessRouter from '@/routes/access.route';
+import postRouter from '@/routes/post.route';
 
 const router = express.Router();
 
 router.use('/access', accessRouter);
+router.use('/post', postRouter);
 
 export default router;
--- src/type.d.ts
+++ src/type.d.ts
@@ -4,6 +4,13 @@ declare global {
   namespace Express {
     interface Request {
       jwtDecoded?: JwtPayload;
+      post?: {
+        validatedQuery: {
+          page: number;
+          limit: number;
+          order: 'asc' | 'desc';
+        };
+      };
     }
   }
 }
