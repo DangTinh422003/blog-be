b59f9efe06813580e8b4efb35b034505c6bf489e
dangtinh <caodangtinh04022003@gmail.com>
2024-12-25 14:53:29 +0700

    update: reset password

--- src/repository/user.repo.ts
+++ src/repository/user.repo.ts
@@ -1,7 +1,11 @@
-import userModel from '@/models/user.model';
+import userModel, { type User } from '@/models/user.model';
 
 export class UserRepository {
   static async findByEmail(email: string) {
     return await userModel.findOne({ email }).lean();
+  }
+
+  static async updateById(userId: string, user: Partial<User>) {
+    return await userModel.findByIdAndUpdate(userId, user);
   }
 }
